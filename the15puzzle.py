0e7d863191bc61a401ddc36d77c5811a920ac2b5
ayamir <lgt986452565@gmail.com>
2021-05-27 21:35:40 +0800

    finished

--- the15puzzle.py
+++ the15puzzle.py
@@ -312,7 +312,6 @@ def get_methods(sequence: list) -> list:
 if __name__ == "__main__":
     # Use 0 as blank
     init = [2, 5, 3, 4, 1, 0, 6, 8, 9, 13, 7, 11, 14, 10, 15, 12]
-    random.shuffle(init)
     goal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
     while not count_inversions(init) % 2 != 0:
         random.shuffle(init)
